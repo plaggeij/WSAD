b14f3775efc466d9481f6c3e9153a0ab22f89b8c
Jake Plagge <plaggeij@mail.uc.edu>
2023-05-21 12:06:28 -0500

    Update Context
    Signed-off-by: Jake Plagge <plaggeij@mail.uc.edu>

--- Assignment 2/Assignment 2/Assignment 2/Data/ForestContext.cs
+++ Assignment 2/Assignment 2/Assignment 2/Data/ForestContext.cs
@@ -1,6 +1,14 @@
-﻿namespace Assignment_2.Data;
+﻿using Assignment_2.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class ForestContext
+namespace Assignment_2.Data;
+
+public class ForestContext: DbContext
 {
-    
+    public ForestContext(DbContextOptions<ForestContext> options):base(options)
+    {
+        
+    }
+    public DbSet<Tree> Trees { get; set; }
+    public DbSet<Branch> Branches { get; set; }
 }
