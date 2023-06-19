24add20e31102a205024c8a2c5d0180c782ab583
Jake Plagge <plaggeij@mail.uc.edu>
2023-06-19 18:12:01 -0500

    Update gitignore, add files that should have been committed
    Signed-off-by: Jake Plagge <plaggeij@mail.uc.edu>

---
+++ Assignment 2/Assignment 2/Assignment 2/Migrations/20230604224359_Tree.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assignment_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tree : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Height",
+                table: "Trees",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "Trees");
+        }
+    }
+}
