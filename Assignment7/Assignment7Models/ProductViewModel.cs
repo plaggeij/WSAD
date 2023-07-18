c502e61cf523d378f3cb2f9b8b288224eadeb7ee
Jake Plagge <plaggeij@mail.uc.edu>
2023-07-18 16:35:38 -0500

    Add changes

---
+++ Assignment7/Assignment7Models/ProductViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Assignment7Models;
+
+public class ProductViewModel
+{
+    public int ProductId { get; set; }
+    public string? ProductName { get; set; }
+    public float ProductPrice { get; set; }
+}
